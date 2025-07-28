72073b736876e4894277661c00180c61e22fa736
vinudasenith <ha.vinudas@gmail.com>
2025-07-28 20:05:32 +0530

    create user model👍

---
+++ src/main/java/com/example/resumeapp/resume_app/model/User.java
@@ -0,0 +1,39 @@
+package com.example.resumeapp.resume_app.model;
+
+import org.springframework.boot.autoconfigure.web.WebProperties.Resources.Chain.Strategy;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false)
+    private String role;
+
+}
