7b096074a738b988472100b67d20dd01e9449701
vinudasenith <ha.vinudas@gmail.com>
2025-07-28 20:00:26 +0530

    init💥

---
+++ src/main/java/com/example/resumeapp/resume_app/ResumeAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.resumeapp.resume_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumeAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/resumeapp/resume_app/ResumeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.resumeapp.resume_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
