8355a7b64a7e63b9f9c2924e5a210ee3f199dfd3
vinudasenith <ha.vinudas@gmail.com>
2025-07-28 20:06:09 +0530

    add user service👍

---
+++ src/main/java/com/example/resumeapp/resume_app/service/UserService.java
@@ -0,0 +1,63 @@
+package com.example.resumeapp.resume_app.service;
+
+import com.example.resumeapp.resume_app.model.User;
+import com.example.resumeapp.resume_app.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public User registerUser(User user) {
+        if (userRepository.findByEmail(user.getEmail()) != null) {
+            throw new RuntimeException("User already exists");
+        } else {
+            user.setPassword(passwordEncoder.encode(user.getPassword()));
+            return userRepository.save(user);
+        }
+    }
+
+    public User loginUser(String email, String password) {
+        User user = userRepository.findByEmail(email);
+        if (user == null) {
+            throw new RuntimeException("User not found");
+        } else {
+            if (passwordEncoder.matches(password, user.getPassword())) {
+                return user;
+            } else {
+                throw new RuntimeException("Invalid password");
+            }
+        }
+    }
+
+    public boolean isAdmin(String email) {
+        User user = userRepository.findByEmail(email);
+        return user.getRole().equals("admin");
+
+    }
+
+    public boolean isCustomer(String email) {
+        User user = userRepository.findByEmail(email);
+        return user.getRole().equals("customer");
+    }
+
+    public List<User> getAllUser() {
+        return userRepository.findAll();
+    }
+
+    public User findByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    public void saveUser(User user) {
+        userRepository.save(user);
+    }
+
+}
