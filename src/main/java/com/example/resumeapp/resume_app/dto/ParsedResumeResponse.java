f19fc4579dbd69d87f8233b5f4dded2393bfc6e1
vinudasenith <ha.vinudas@gmail.com>
2025-08-04 16:53:48 +0530

    add  parsed resum response entity👍

---
+++ src/main/java/com/example/resumeapp/resume_app/dto/ParsedResumeResponse.java
@@ -0,0 +1,36 @@
+package com.example.resumeapp.resume_app.dto;
+
+import java.util.List;
+import java.util.Map;
+
+public class ParsedResumeResponse {
+    private Map<String, Object> parsed_data;
+    private Map<String, Object> resume_score;
+    private String raw_text;
+
+    // Getters and setters
+
+    public Map<String, Object> getParsed_data() {
+        return parsed_data;
+    }
+
+    public void setParsed_data(Map<String, Object> parsed_data) {
+        this.parsed_data = parsed_data;
+    }
+
+    public Map<String, Object> getResume_score() {
+        return resume_score;
+    }
+
+    public void setResume_score(Map<String, Object> resume_score) {
+        this.resume_score = resume_score;
+    }
+
+    public String getRaw_text() {
+        return raw_text;
+    }
+
+    public void setRaw_text(String raw_text) {
+        this.raw_text = raw_text;
+    }
+}
