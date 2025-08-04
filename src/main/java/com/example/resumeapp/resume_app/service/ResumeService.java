09d0703649078fca1d8bd79b99d5919a72e00c7f
vinudasenith <ha.vinudas@gmail.com>
2025-08-04 16:55:33 +0530

    add method to send resume to fast api👍

--- src/main/java/com/example/resumeapp/resume_app/service/ResumeService.java
+++ src/main/java/com/example/resumeapp/resume_app/service/ResumeService.java
@@ -1,9 +1,14 @@
 package com.example.resumeapp.resume_app.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 
 import com.example.resumeapp.resume_app.model.Resume;
 import com.example.resumeapp.resume_app.repository.ResumeRepository;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpEntity;
 
 import lombok.RequiredArgsConstructor;
 
@@ -11,11 +16,20 @@ import com.example.resumeapp.resume_app.model.User;
 
 import java.util.List;
 
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+
 @Service
 @RequiredArgsConstructor
 public class ResumeService {
 
     private final ResumeRepository resumeRepository;
+
+    private final RestTemplate restTemplate = new RestTemplate();
+    private final String fastApiUrl = "http://localhost:8000/parse_resume";
 
     public Resume saveResume(Resume resume) {
         return resumeRepository.save(resume);
@@ -33,4 +47,25 @@ public class ResumeService {
         return resumeRepository.findByFileName(fileName);
     }
 
+    public String sendResumeToFastApi(MultipartFile resumeFile) throws Exception {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+
+        ByteArrayResource contetArrayResource = new ByteArrayResource(resumeFile.getBytes()) {
+            @Override
+            public String getFilename() {
+                return resumeFile.getOriginalFilename();
+            }
+        };
+
+        MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
+        body.add("file", contetArrayResource);
+
+        HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body, headers);
+
+        ResponseEntity<String> response = restTemplate.postForEntity(fastApiUrl, requestEntity, String.class);
+
+        return response.getBody();
+
+    }
 }
