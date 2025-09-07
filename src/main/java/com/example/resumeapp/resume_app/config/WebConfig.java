e710cd1807b8a0fc2327048a9e852d474bad149d
vinudasenith <ha.vinudas@gmail.com>
2025-09-07 20:30:22 +0530

    add webConfig✔️

---
+++ src/main/java/com/example/resumeapp/resume_app/config/WebConfig.java
@@ -0,0 +1,15 @@
+package com.example.resumeapp.resume_app.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations("file:C:/Users/Vinuda/Documents/Project/uploads/");
+    }
+}
