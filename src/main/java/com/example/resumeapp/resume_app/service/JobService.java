798ccef09bd66b07f6d7254ea2f318c983fed133
vinudasenith <ha.vinudas@gmail.com>
2025-09-10 22:07:25 +0530

    add comments👍

--- src/main/java/com/example/resumeapp/resume_app/service/JobService.java
+++ src/main/java/com/example/resumeapp/resume_app/service/JobService.java
@@ -15,26 +15,32 @@ public class JobService {
 
     private final JobRepository jobRepository;
 
+    // Create a new job
     public Job createJob(Job job) {
         return jobRepository.save(job);
     }
 
+    // Get all jobs
     public List<Job> getAllJobs() {
         return jobRepository.findAll();
     }
 
+    // Get job by ID
     public Optional<Job> getJobById(Long id) {
         return jobRepository.findById(id);
     }
 
+    // get jobs posted
     public List<Job> getJobsByPoster(String email) {
         return jobRepository.findByPostedBy(email);
     }
 
+    // Delete job
     public void deleteJob(Long id) {
         jobRepository.deleteById(id);
     }
 
+    // Search by title
     public List<Job> searchJobsByTitle(String title) {
         return jobRepository.findByTitleContainingIgnoreCase(title);
     }
