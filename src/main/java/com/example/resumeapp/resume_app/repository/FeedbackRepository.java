34cf3b45d80f6fdb15ac756a0e0021f784f97c78
vinudasenith <ha.vinudas@gmail.com>
2025-09-11 19:52:30 +0530

    update feedback repository with user eamil query✔️

--- src/main/java/com/example/resumeapp/resume_app/repository/FeedbackRepository.java
+++ src/main/java/com/example/resumeapp/resume_app/repository/FeedbackRepository.java
@@ -3,12 +3,19 @@ package com.example.resumeapp.resume_app.repository;
 import com.example.resumeapp.resume_app.model.Feedback;
 import com.example.resumeapp.resume_app.model.Resume;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface FeedbackRepository extends JpaRepository<Feedback, Long> {
-    Optional<Feedback> findByResume(Resume resume);
+
+    List<Feedback> findByResume(Resume resume);
+
+    @Query("SELECT f FROM Feedback f WHERE f.resume.user.email = :email")
+    List<Feedback> findAllByResumeUserEmail(@Param("email") String email);
 
 }
